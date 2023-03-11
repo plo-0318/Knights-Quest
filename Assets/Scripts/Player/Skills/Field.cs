6b6613e4a4f99a2d41c1a11d801885419e9e7202
awesomeaj5 <43348940+awesomeaj5@users.noreply.github.com>
2023-03-10 21:10:48 -0800

    Added field skill

--- Assets/Scripts/Player/Skills/Field.cs
+++ Assets/Scripts/Player/Skills/Field.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Field : MonoBehaviour
 {
- 
-  private float damage;
+    private Vector3 BASE_SCALE;
+    private float damage;
 
-[SerializeField]
-  private float size;
-
-   public void Init(float damage, float size)
+    private void Start()
     {
-        this.damage = damage;
-        this.size = size;
+        BASE_SCALE = new Vector3(
+            transform.localScale.x,
+            transform.localScale.y,
+            transform.localScale.z
+        );
     }
 
+    public void Init(float damage, float size)
+    {
+        this.damage = damage;
+    }
+
+    public void SetScale(float multiplier)
+    {
+        transform.localScale = BASE_SCALE * multiplier;
+    }
 }
--- Assets/Scripts/Player/Skills/SkillField.cs
+++ Assets/Scripts/Player/Skills/SkillField.cs
@@ -4,54 +4,57 @@ using UnityEngine;
 
 public class SkillField : Skill
 {
-    private GameObject Field;
-    [SerializeField] public GameObject Player;
+    private Field field;
+    private GameObject FieldPrefab;
     private const float BASE_DAMAGE = 1f;
-    private  Vector2 BASE_SIZE = new Vector2(4f,4f);
     private float damage;
-    private Vector2 size;
-    
+    private bool hasField;
 
     public SkillField()
     {
         name = "Field";
-        Field = Resources.Load<GameObject>("field");
+        FieldPrefab = Resources.Load<GameObject>("Field");
 
         level = 1;
         damage = BASE_DAMAGE;
 
-        size = BASE_SIZE;
-
+        hasField = false;
     }
-
 
     public override void Upgrade()
     {
         base.Upgrade();
 
-        if(level == 2)
+        if (level == 2)
         {
             damage = BASE_DAMAGE * 2f;
-            size = BASE_SIZE * 1.25f;
+            field.SetScale(1.2f);
         }
 
-        if(level == 3)
+        if (level == 3)
         {
             damage = BASE_DAMAGE * 3f;
-           size = BASE_SIZE * 2f;
+            field.SetScale(1.4f);
         }
 
-        if(level == 4)
+        if (level == 4)
         {
             damage = BASE_DAMAGE * 4f;
-            size = BASE_SIZE * 3f;
+            field.SetScale(1.6f);
+        }
     }
-}
-private void OnTriggerEnter2D(Collider2D other)
-{
-    if(other.CompareTag("Player"))
+
+    public override void Use()
     {
-        other.transform.parent = Player.transform;
+        //Instantiate Forcefield here, but only if there isn't one already.
+        if (!hasField)
+        {
+            Vector2 playerPos = GameManager.PlayerMovement().transform.position;
+            field = GameObject
+                .Instantiate(FieldPrefab, playerPos, Quaternion.identity)
+                .GetComponent<Field>();
+            hasField = true;
+            field.transform.parent = GameManager.PlayerMovement().transform;
+        }
     }
-}
 }
