e4ded6f2f4988f5e93ffb49fa5c36646bcc504a2
Gabrielius Gintalas <gabrieliusgintalas@csu.fullerton.edu>
2023-04-14 10:31:20 -0700

    Randomized radius and fixed spawning outside of map

--- Assets/Scripts/Collectable/CollectableBarrelSpawner.cs
+++ Assets/Scripts/Collectable/CollectableBarrelSpawner.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CollectableBarrelSpawner : MonoBehaviour
 {
     public GameObject barrelPref;
     public Transform player; 
-    public float radius = 10f;
+    public float minRadius = 10f;
+    public float maxRadius = 15f;
     public float interval = 4f;
+    public Tilemap gameMap;
     
     // Start is called before the first frame update
     void Start()
@@ -16,16 +19,28 @@ public class CollectableBarrelSpawner : MonoBehaviour
     }
     
     private void SpawnBarrel(){
+        float radius = Random.Range(minRadius, maxRadius);
         Vector2 randomDirection = Random.insideUnitCircle.normalized * radius;
         Vector3 spawnPos = new Vector3(player.position.x + randomDirection.x, player.position.y + randomDirection.y, player.position.z);
 
-        GameObject spawnedBarrel = Instantiate(barrelPref, spawnPos, Quaternion.identity);
-
-         CollectableSpawner collectableSpawner = spawnedBarrel.GetComponent<CollectableSpawner>();
-        if (collectableSpawner != null)
+        if(IsWithinBounds(spawnPos))
         {
-            collectableSpawner.SpawnRandomCollectable(spawnPos, Quaternion.identity);
+            GameObject spawnedBarrel = Instantiate(barrelPref, spawnPos, Quaternion.identity);
+
+            CollectableSpawner collectableSpawner = spawnedBarrel.GetComponent<CollectableSpawner>();
+            if (collectableSpawner != null)
+            {
+                collectableSpawner.SpawnRandomCollectable(spawnPos, Quaternion.identity);
+            }
         }
+    }
+
+    private bool IsWithinBounds(Vector3 position)
+    {
+       BoundsInt mapBounds = gameMap.cellBounds;
+       Vector3Int cellPosition = gameMap.WorldToCell(position);
+
+       return mapBounds.Contains(cellPosition);
     }
 
 }
