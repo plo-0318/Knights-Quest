7490c391cb29118a372806c70061ffc39321fc79
plo-0318 <plo03180220@gmail.com>
2023-04-14 23:17:10 -0700

    commented out spawning extra collectable, updated barrel prefab rigidbody

--- Assets/Scripts/Collectable/Barrel.cs
+++ Assets/Scripts/Collectable/Barrel.cs
@@ -7,32 +7,45 @@ public class Barrel : MonoBehaviour
     private Animator anim;
     private CollectableSpawner collectableSpawner;
 
-    private void Awake() {
+    [SerializeField]
+    private Transform point1,
+        point2;
+
+    private void Awake()
+    {
         anim = GetComponent<Animator>();
         collectableSpawner = GetComponent<CollectableSpawner>();
     }
 
-    private void OnTriggerEnter2D(Collider2D other) 
+    private bool Broken()
     {
-        if(other.GetComponent<PlayerMovement>() != null) {
+        return Physics2D.OverlapArea(point1.position, point2.position, LayerMask.GetMask("Skill"));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerMovement>() != null)
+        {
             return;
         }
 
         // It is a skill, add logic for breaking the chest
-        Hammer hammer = other.GetComponent<Hammer>();
-        Sword sword = other.GetComponent<Sword>();
-        Dagger dagger = other.GetComponent<Dagger>();
-        if (hammer != null || sword != null || dagger != null)
-        {
-            anim.SetTrigger("Destroy");
-            StartCoroutine(SpawnCollectableAfterAnimation(0.4f));
-        }
+        // Hammer hammer = other.GetComponent<Hammer>();
+        // Sword sword = other.GetComponent<Sword>();
+        // Dagger dagger = other.GetComponent<Dagger>();
+        // if (hammer != null || sword != null || dagger != null)
+        // {
+        //     anim.SetTrigger("Destroy");
+        //     StartCoroutine(SpawnCollectableAfterAnimation(0.4f));
+        // }
+
+        anim.SetTrigger("Destroy");
+        StartCoroutine(SpawnCollectableAfterAnimation(0.4f));
     }
 
     private IEnumerator SpawnCollectableAfterAnimation(float delay)
     {
         yield return new WaitForSeconds(delay);
-        Debug.Log("Should spawn object");
         collectableSpawner.SpawnRandomCollectable(transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
--- Assets/Scripts/Collectable/CollectableBarrelSpawner.cs
+++ Assets/Scripts/Collectable/CollectableBarrelSpawner.cs
@@ -6,42 +6,46 @@ using UnityEngine.Tilemaps;
 public class CollectableBarrelSpawner : MonoBehaviour
 {
     public GameObject barrelPref;
-    public Transform player; 
+    public Transform player;
     public float minRadius = 10f;
     public float maxRadius = 15f;
     public float interval = 4f;
     public Tilemap gameMap;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating(nameof(SpawnBarrel), 0f, interval);
     }
-    
-    private void SpawnBarrel(){
+
+    private void SpawnBarrel()
+    {
         float radius = Random.Range(minRadius, maxRadius);
         Vector2 randomDirection = Random.insideUnitCircle.normalized * radius;
-        Vector3 spawnPos = new Vector3(player.position.x + randomDirection.x, player.position.y + randomDirection.y, player.position.z);
+        Vector3 spawnPos = new Vector3(
+            player.position.x + randomDirection.x,
+            player.position.y + randomDirection.y,
+            player.position.z
+        );
 
-        if(IsWithinBounds(spawnPos))
+        if (IsWithinBounds(spawnPos))
         {
             GameObject spawnedBarrel = Instantiate(barrelPref, spawnPos, Quaternion.identity);
 
-            CollectableSpawner collectableSpawner = spawnedBarrel.GetComponent<CollectableSpawner>();
-            if (collectableSpawner != null)
-            {
-                collectableSpawner.SpawnRandomCollectable(spawnPos, Quaternion.identity);
-            }
+            CollectableSpawner collectableSpawner =
+                spawnedBarrel.GetComponent<CollectableSpawner>();
+            // if (collectableSpawner != null)
+            // {
+            //     collectableSpawner.SpawnRandomCollectable(spawnPos, Quaternion.identity);
+            // }
         }
     }
 
     private bool IsWithinBounds(Vector3 position)
     {
-       BoundsInt mapBounds = gameMap.cellBounds;
-       Vector3Int cellPosition = gameMap.WorldToCell(position);
+        BoundsInt mapBounds = gameMap.cellBounds;
+        Vector3Int cellPosition = gameMap.WorldToCell(position);
 
-       return mapBounds.Contains(cellPosition);
+        return mapBounds.Contains(cellPosition);
     }
-
 }
-
