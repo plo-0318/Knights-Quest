5961275d7630c42c1adc8226b0630afd2dca34e0
jethanstory <32407746+jethanstory@users.noreply.github.com>
2023-04-28 14:11:37 -0700

    Fixed angle aspect

--- Assets/Scripts/Enemies/EnemyProjectile.cs
+++ Assets/Scripts/Enemies/EnemyProjectile.cs
@@ -47,25 +47,21 @@ public class EnemyProjectile : MonoBehaviour
         Vector3 objectPos = transform.position;
         // float angleBetweenPoints = Util.GetNormalizedAngle(point1, point2);
         float angleBetweenPoints = Util.GetNormalizedAngle(objectPos, playerPos);
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angleBetweenPoints - 45f));
-        rb.AddForce(transform.up * speed); //* Time.deltaTime   
+        // transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angleBetweenPoints - 45f));
+        // rb.AddForce(transform.up * speed); //* Time.deltaTime 
+        transform.rotation = Quaternion.Euler(0, 0, angleBetweenPoints);
+
+        Vector2 direction = new Vector2(Mathf.Cos(angleBetweenPoints * Mathf.Deg2Rad), Mathf.Sin(angleBetweenPoints * Mathf.Deg2Rad));
+
+        rb.velocity = direction * speed;
     }
 
-
-
-    
-
-    // void HurtPlayer()
-    // {
-        
-    // }
-
-    // private void OnTriggerEnter2D(Collider2D other)
-    // {
-    //     if (other.TryGetComponent<Player>(out Player PlayerCollider))
-    //     {
-    //        // hurt the player
-    //        GameManager.PlayerStatus().Hurt(100f); 
-    //     }
-    // }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent<Player>(out Player PlayerCollider))
+        {
+           // hurt the player
+           GameManager.PlayerStatus().Hurt(100f); 
+        }
+    }
 }
