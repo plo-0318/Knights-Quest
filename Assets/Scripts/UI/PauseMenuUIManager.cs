3625f939097b0183ef2d1d6ee47c7c21de827792
Carlos Mora <58601922+CGM-Charlie@users.noreply.github.com>
2023-05-01 23:32:34 -0700

    Update skill info in pause menu

--- Assets/Scripts/UI/PauseMenuUIManager.cs
+++ Assets/Scripts/UI/PauseMenuUIManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
@@ -7,7 +9,6 @@ using UnityEngine.UI;
 
 public class PauseMenuUIManager : MonoBehaviour 
 {
-    
     [Header("Pause Screens")]    
     [SerializeField] private GameObject pauseMenuScreen;
     [SerializeField] private GameObject optionsScreen;
@@ -21,10 +22,18 @@ public class PauseMenuUIManager : MonoBehaviour
 
     [Header("Options Menu Buttons")] 
     [SerializeField] private GameObject optionsBackButton;
+    
+    [Header("Pause Menu Skills")]
+    [SerializeField] private GameObject[] combatSkills;
+    [SerializeField] private GameObject[] utilitySkills;
+
+    private PlayerStatus playerStatus;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+        // Setup Player Status
+        playerStatus = GameManager.PlayerStatus();
+        
         // Pause Menu - Listeners
         var continueBtn = continueButton.GetComponent<Button>();
         var continueBtnEvents = continueButton.GetComponent<ButtonEventHandler>();
@@ -65,14 +74,65 @@ public class PauseMenuUIManager : MonoBehaviour
         optionsScreen.SetActive(true);
     }
 
-    // Public Functions
+    private void UpdateSkillUI() 
+    {
+        var combatSkillData = playerStatus.GetSkillDatum().Where(c => c.Type == "ATTACK").ToArray();
+        
+        for (var i = 0; i < combatSkills.Length; i++) {
+            var skillData = (combatSkillData.Length > i) ? combatSkillData[i] : null;
+            var currentSkill = combatSkills[i];
 
+            var skillTitle = currentSkill.transform.Find("SkillTitle").gameObject.GetComponent<TextMeshProUGUI>();
+            var skillDescription = currentSkill.transform.Find("SkillDescription").gameObject.GetComponent<TextMeshProUGUI>();
+            skillTitle.text = (skillData != null) ? skillData.DisplayName : "";
+            skillDescription.text = (skillData != null) ? skillData.Description : "";
+            
+            var skillLevelPlaceholder = currentSkill.transform.Find("Skill/LevelPlaceholder").gameObject.GetComponent<Image>();
+            var skillIcon = currentSkill.transform.Find("Skill/SkillIcon").gameObject.GetComponent<Image>();
+            var skillLevelLabel = currentSkill.transform.Find("Skill/LevelLabel").GetComponent<TextMeshProUGUI>();
+            skillLevelPlaceholder.color = new Color(255, 255, 255, (skillData != null) ? 1f : 0f);
+            skillIcon.sprite = (skillData != null) ? skillData.Sprite : null;
+            skillIcon.color = new Color(255, 255, 255, (skillData != null) ? 1f : 0f);
+            skillLevelLabel.color = new Color(255, 255, 255, (skillData != null) ? 1f : 0f);
+            if (skillData != null) skillLevelLabel.text = skillData.GetCurrentLevel().ToString();
+        }
+        
+        var utilitySkillData = playerStatus.GetSkillDatum().Where(c => c.Type == "UTILITY").ToArray();
+
+        for (var i = 0; i < utilitySkills.Length; i++) {
+            var skillData = (utilitySkillData.Length > i) ? utilitySkillData[i] : null;
+            var currentSkill = utilitySkills[i];
+
+            var skillTitle = currentSkill.transform.Find("SkillTitle").gameObject.GetComponent<TextMeshProUGUI>();
+            var skillDescription = currentSkill.transform.Find("SkillDescription").gameObject.GetComponent<TextMeshProUGUI>();
+            skillTitle.text = (skillData != null) ? skillData.DisplayName : "";
+            skillDescription.text = (skillData != null) ? skillData.Description : "";
+            
+            var skillLevelPlaceholder = currentSkill.transform.Find("Skill/LevelPlaceholder").gameObject.GetComponent<Image>();
+            var skillIcon = currentSkill.transform.Find("Skill/SkillIcon").gameObject.GetComponent<Image>();
+            var skillLevelLabel = currentSkill.transform.Find("Skill/LevelLabel").GetComponent<TextMeshProUGUI>();
+            skillLevelPlaceholder.color = new Color(255, 255, 255, (skillData != null) ? 1f : 0f);
+            skillIcon.sprite = (skillData != null) ? skillData.Sprite : null;
+            skillIcon.color = new Color(255, 255, 255, (skillData != null) ? 1f : 0f);
+            skillLevelLabel.color = new Color(255, 255, 255, (skillData != null) ? 1f : 0f);
+            if (skillData != null) skillLevelLabel.text = skillData.GetCurrentLevel().ToString();
+        }
+    }
+
+    // Public Functions
     public void ShowPauseMenu() {
+
+        Time.timeScale = 0;
+        
+        UpdateSkillUI();
+        
         var menuAnimation = gameObject.GetComponent<InAndOutAnimation>();
         menuAnimation.MoveInAnimation();
     }
 
     public void HidePauseMenu() {
+        Time.timeScale = 1;
+
         var menuAnimation = gameObject.GetComponent<InAndOutAnimation>();
         menuAnimation.MoveOutAnimation();
     }
